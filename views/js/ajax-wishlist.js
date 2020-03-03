0c5a3becc0ddf1470f1ce363738a9318ba8cf862
Ko_On <Ko_On@DESKTOP-C9G95IE>
2020-03-03 23:20:29 +0100

    license js

--- views/js/ajax-wishlist.js
+++ views/js/ajax-wishlist.js
@@ -1,3 +1,17 @@
+/**
+ * 2015-2020 Ko_Ondziu
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    Ko_Ondziu <000konrad000@gmail.com>
+ * @copyright 2015-2020 Ko_Ondziu
+ * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
+ */
+
 // var wishlistProductsIds = [];
 
 const baseDir = prestashop.urls.base_url;
